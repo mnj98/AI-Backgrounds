2255806f400a8c3b181e136b38d715f2af6952d8
Matthew Jackson <mnj98@vt.edu>
2023-02-27 12:55:40 -0500

    created background generator component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { GenBackgroundComponent } from './gen-background/gen-background.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    GenBackgroundComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/gen-background/gen-background.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gen-background',
+  templateUrl: './gen-background.component.html',
+  styleUrls: ['./gen-background.component.css']
+})
+export class GenBackgroundComponent {
+
+}
